2af7ffd2026465c609a863a0de93bad9c1110734
CoderGUY47 <shkhridoy92@gmail.com>
2026-06-24 13:23:06 +0600

    explain what typescript is and why it is needed

---
+++ index.ts
@@ -0,0 +1,21 @@
+// =========================
+//        (10 TASKS)
+// =========================
+
+export {};
+
+////////*****==============\\\\\\\\
+// 1. WHAT IS TYPESCRIPT
+//\\\\\\*****==============////////
+// typescript acts as a wrapper around javascript that checks types at compile time.
+console.log("=== 1. WHAT IS TYPESCRIPT ===");
+console.log("TypeScript is a safer version of JavaScript that catches errors early.");
+
+
+
+////////*****==============\\\\\\\\
+// 2. WHY TYPESCRIPT NEED?
+//\\\\\\*****==============////////
+// it prevents common errors like typos, wrong variables, or passing wrong arguments to functions.
+console.log("\n=== 2. WHY TYPESCRIPT NEED? ===");
+console.log("It helps us find bugs before running our code, making development easier.");
